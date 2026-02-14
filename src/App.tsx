478334d4d02c534d93390dab9f59b51136e45165
Baronman <failingitup@gmail.com>
2026-02-14 15:05:11 -0500

    Use HashRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+
 import RecipeListPage from "./pages/RecipeListPage";
 import NewRecipePage from "./pages/NewRecipePage";
 import RecipeDetailPage from "./pages/RecipeDetailPage";
@@ -6,13 +8,13 @@ import EditRecipePage from "./pages/EditRecipePage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<RecipeListPage />} />
         <Route path="/recipes/new" element={<NewRecipePage />} />
         <Route path="/recipes/:id" element={<RecipeDetailPage />} />
         <Route path="/recipes/:id/edit" element={<EditRecipePage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
